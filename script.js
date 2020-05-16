29342cfac488520001a84dd6745c0f4aa467adbd
awersmanser <65021591+awersmanser@users.noreply.github.com>
2020-05-16 19:01:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let select = 0;
+let show = true;
+
 window.onload = function () {
     document.querySelector('.auth-button').style.transition = 'all .15s ease';
     document.querySelector('#auth-circle-login').style.transition = 'all .5s ease';
     document.querySelector('#auth-circle-password').style.transition = 'all .5s ease';
-    document.querySelector('.star-object').style.transition = 'all .25s ease-in-out';
+    document.querySelector('.star-object').style.transition = 'all .3s ease-in-out';
+    document.querySelector('.back-button').style.transition = 'all .25s ease-in-out';
     document.querySelector('.modal-notification-window').style.transition = 'all .25s ease';
     document.querySelector('.modal-notification-window-content').style.transition = 'all .25s ease-in-out';
     document.querySelector('.modal-notification-window-close-button').style.transition = 'all .25s ease';
@@ -10,28 +14,37 @@ window.onload = function () {
     callNotification();
     closeNotification();
     setTimeout(() => { document.querySelector('.preloader').style.opacity = '0', document.querySelector('.preloader').style.visibility = 'hidden' }, 500);
+    setTimeout(() => { document.querySelector('#gif-loading').style.opacity = '0', document.querySelector('.gif').style.background = 'url(images/gif_background.gif)' }, 1500);
+    show=getCookie(show);
 }
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
 
 function authCheck(login, password, authLog) {
     if (document.querySelector('#login').value == 'asdfG1') {
-        document.querySelector('#auth-circle-login').style.background = '#32db05';
+        document.querySelector('#auth-circle-login').style.background = 'rgb(20, 223, 30)';
         login = true;
     } else {
         if (document.querySelector('#login').value != '') {
-            document.querySelector('#auth-circle-login').style.background = '#ff0000';
+            document.querySelector('#auth-circle-login').style.background = 'rgb(255, 62, 62)';
         } else {
-            document.querySelector('#auth-circle-login').style.background = '#808080';
+            document.querySelector('#auth-circle-login').style.background = 'rgb(128, 128, 128)';
         }
         login = false;
     }
     if (document.querySelector('#password').value == 'qwertY!123z') {
-        document.querySelector('#auth-circle-password').style.background = '#32db05';
+        document.querySelector('#auth-circle-password').style.background = 'rgb(20, 223, 30)';
         password = true;
     } else {
         if (document.querySelector('#password').value != '') {
-            document.querySelector('#auth-circle-password').style.background = '#ff0000';
+            document.querySelector('#auth-circle-password').style.background = 'rgb(255, 62, 62)';
         } else {
-            document.querySelector('#auth-circle-password').style.background = '#808080';
+            document.querySelector('#auth-circle-password').style.background = 'rgb(128, 128, 128)';
         }
         password = false;
     }
@@ -75,7 +88,7 @@ function callNotification(name, text, button) {
     document.querySelector('.modal-notification-window').style.visibility = 'visible';
     document.querySelector('.modal-notification-window-content').style.transform = 'scale(1) translateY(0)';
     document.querySelector('.modal-notification-window').style.backdropFilter = 'blur(5px)';
-    document.querySelector('.modal-notification-window').style.background = 'rgba(0, 0, 0, 0.5)';
+    document.querySelector('.modal-notification-window').style.background = 'rgba(0, 0, 0, .5)';
     document.querySelector('.modal-notification-window-header-title').textContent = name;
     document.querySelector('.modal-notification-window-text').textContent = text;
     document.querySelector('.modal-notification-window-accept-button').textContent = button;
@@ -83,74 +96,65 @@ function callNotification(name, text, button) {
 
 function closeNotification() {
     document.body.style.overflow = 'visible';
-    document.querySelector('.modal-notification-window-content').style.transform = 'scale(1.5) translateY(-500%)';
+    document.querySelector('.modal-notification-window-content').style.transform = 'scale(2) translateY(-1000%)';
     document.querySelector('.modal-notification-window').style.backdropFilter = 'blur(0px)';
     document.querySelector('.modal-notification-window').style.background = 'transparent';
     document.querySelector('.modal-notification-window').style.visibility = 'hidden';
-}
-
-document.querySelector('#login').onfocus = () => {
-    document.onkeydown = (e) => {
-        if (e.keyCode === 13) {
-            if (authCheck().includes('Login: true')) {
-                document.querySelector('#password').focus();
-            } else { authAttention() }
-        }
-    }
-}
-
-document.querySelector('#password').onfocus = () => {
-    document.onkeydown = (e) => {
-        if (e.keyCode === 13) {
-            if (authCheck().includes('password: true')) {
-                document.querySelector('#password').blur();
-                start();
-            } else { authAttention() }
-        }
-    }
-}
-
-document.querySelector('#login').onblur = () => {
-    document.querySelector('#password').onblur = () => {
+    if (select == 'login') {
         document.querySelector('#login').focus();
-        document.querySelector('#login').blur();
+    }
+    if (select == 'password') {
+        document.querySelector('#password').focus();
     }
 }
 
-document.querySelector('.modal-notification-window-background').onmouseover = () => document.querySelector('.modal-notification-window-close-button').style.background = '#ff3e3e'
-document.querySelector('.modal-notification-window-background').onmousedown = () => document.querySelector('.modal-notification-window-close-button').style.background = '#eb1515'
+document.querySelector('.modal-notification-window-background').onmouseover = () => document.querySelector('.modal-notification-window-close-button').style.background = 'rgb(255, 62, 62)'
+document.querySelector('.modal-notification-window-background').onmousedown = () => document.querySelector('.modal-notification-window-close-button').style.background = 'rgb(235, 21, 21)'
 document.querySelector('.modal-notification-window-background').onmouseleave = () => document.querySelector('.modal-notification-window-close-button').style.background = ''
 document.querySelector('.auth-button').onclick = () => start()
 document.querySelector('.modal-notification-window-close-button').onclick = () => closeNotification()
 document.querySelector('.modal-notification-window-accept-button').onclick = () => closeNotification()
 document.querySelector('.modal-notification-window-background').onclick = () => closeNotification()
-document.querySelector('.back-button').onclick = () => document.querySelector('.star').style.display = 'none', document.querySelector('.star').style.visibility = 'hidden';
-setTimeout(() => { document.querySelector('.star').style.display = 'inline' }, 500);
-
-document.onkeydown = (e) => {
-    if (e.keyCode === 13) {
-        closeNotification();
-    }
-    if (e.keyCode === 27) {
-        closeNotification();
-    }
-}
+document.querySelector('.back-button').onclick = () => document.querySelector('.star').style.display = 'none';
 
 function start() {
     document.querySelector('.auth-button').blur();
     if (authCheck().includes('Login: true') & authCheck().includes('password: true')) {
-        document.querySelector('.star').style.visibility = 'visible';
-        callNotification('Notification', 'Random 10 numbers from 0 to 9 and using as coefficient for determination of star luminosity. Creates fire effect.', 'OK');
+        document.querySelector('.star').style.display = 'flex';
+        if (show) { callNotification('Notification', 'Random 10 numbers from 0 to 9 generating and using as coefficient for determination of star luminosity. Creates fire effect.', 'OK'); 
+        show = false;
+        document.cookie = "show=false";
+    }
     } else { authAttention() }
 }
 
+document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        document.querySelector('.modal-notification-window-close-button').style.background = 'rgb(235, 21, 21)'
+        closeNotification();
+    }
+    if (evt.keyCode == 13) {
+        document.querySelector('.modal-notification-window-close-button').style.background = 'rgb(235, 21, 21)';
+        closeNotification();
+
+    }
+};
+
+document.querySelector('#login').onfocus = () => {
+    select = 'login';
+}
+
+document.querySelector('#password').onfocus = () => {
+    select = 'password';
+}
+
 setInterval(() => {
-    let letters = '0123456789';
+    let numbers = '0123456789';
     let number = 0;
     for (let i = 0; i < 10; i++) {
-        number = letters[Math.floor(Math.random() * 10)];
+        number = numbers[Math.floor(Math.random() * 10)];
     }
-    document.querySelector('.star-object').style.transform = `scale(1.${number})`;
-    document.querySelector('.star-object').style.boxShadow = `0 0 100px ${number * number}px #f7f${number}e9`;
-    console.log(number * number);
-}, 250)
+    document.querySelector('.star-object').style.transform = `scale(1.${Math.round(number / 4)})`;
+    document.querySelector('.star-object').style.boxShadow = `0 0 100px ${Math.pow(number, 2)}px rgb(24${number}, 24${number}, 23${number})`;
+}, 300)
